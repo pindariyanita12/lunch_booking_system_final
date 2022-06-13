44fda468fe149cd65024761b1644adb29dc22eb3
pindariyanita12 <nita.p@simformsolutions.com>
2022-06-13 10:21:01 +0530

    added csv export button in employee page

--- resources/views/admin/employee.blade.php
+++ resources/views/admin/employee.blade.php
@@ -9,15 +9,30 @@
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.dataTables.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.9/sweetalert2.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.9/sweetalert2.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"
+        integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" crossorigin="anonymous"></script>
+    <script src=" https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>
+    <script src=" https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
     <style>
+        .dt-button,
+        .buttons-csv,
+        .buttons-html5 {
+            margin-left: 10px;
+            margin-bottom: 20px;
+        }
+
         h3 {
             text-align: center;
             margin-top: 35px;
@@ -52,7 +67,7 @@
         </div>
     </div>
     <div class="container">
-        <table class="table table-bordered data-table">
+        <table class="table table-bordered data-table" id="dataTable">
             <thead>
                 <tr>
                     <th>{{ trans('home.titleempid') }}</th>
@@ -155,9 +170,23 @@
     function initPage() {
         var table = $('.data-table').DataTable({
             processing: true,
-            serverSide: true,
+            serverSide: false,
             responsive: true,
+            dom: 'lrBfrtip',
             ajax: "{{ route('user.list') }}",
+            buttons: [
+
+                {
+                    extend: 'csv',
+                    text: 'Export CSV',
+                    className: 'btn-space',
+                    exportOptions: {
+                        orthogonal: null,
+                        columns: [0, 1, 2]
+                    }
+                }
+
+            ],
             columns: [{
                     data: 'emp_id',
                     name: 'emp_id',
